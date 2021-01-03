978bc6e64a59c062686d775706da07b2b052e947
thepaulyc <craymer43@me.com>
2021-01-03 17:40:17 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi There! Edit <code>your code numpty</code> and save to reload. That was a Ball Ache!! Why does shit keep moving?
         </p>
         <a
           className="App-link"
